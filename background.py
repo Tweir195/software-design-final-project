770a2a78287a1eafded832c3fb8373745970c290
Tweir195 <tommy.weir@students.olin.edu>
2018-04-07 18:26:25 -0400

    Have a working background class, putting things together in main.py

--- background.py
+++ background.py
@@ -9,17 +9,30 @@ class background(object):
         self.screen = pygame.display.set_mode(size)
         self.background = pygame.Surface(self.screen.get_size())
         self.images = images
-    def display_image(self, index):
+    def clear_screen(self):
+        self.background.fill((0,0,0))
+        pygame.display.update()
+    def display_image(self, index, x, y):
         image = pygame.image.load(self.images[index])
         self.image = pygame.transform.scale(image, (1280,960))
+        self.background.blit(self.image,(x,y))
+    def draw(self, index,x,y):
+        self.screen.blit(view.background,(0,0))
+        self.clear_screen()
+        self.display_image(index,x,y)
+        pygame.display.update()
 
 if __name__ == "__main__":
     pygame.init()
-    view = background(["example1.png"], size=(1280,960))
-    view.display_image(0)
+    view = background(["Attachment-1.png"], size=(1280,960))
     running = True
     view.screen.blit(view.background,(0,0))
+    view.clear_screen()
+    view.display_image(0, 0,0)
+    pygame.display.update()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        view.draw(0,0,0)
+        view.clear_screen()
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #This is the main file for our project
+from background.py import *
+from informationclasses.py import *
+
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+    view = background(["Attachment-1.png"], size=(1280,960))
+    running = True
+    view.screen.blit(view.background,(0,0))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        view.draw(0,0,0)
+        view.clear_screen()

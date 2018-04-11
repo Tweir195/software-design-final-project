94c0a9783738854d11a736815f1274d9198a944a
Tweir195 <tommy.weir@students.olin.edu>
2018-04-10 20:00:30 -0400

    Completed a working animation of a small cat image

--- main.py
+++ main.py
@@ -56,18 +56,51 @@ class real_picture:
         view.screen.blit(self.image,(self.rightl,self.leftl))
         pygame.display.update()
 
+
+class sprite:
+
+
+    def __init__(self,image, x, y):
+        self.image = pygame.image.load(image)
+        self.x = x
+        self.y = y
+        self.state = 0
+    def draw(self):
+        """ intserts real image onto screen"""
+        view.screen.blit(self.image,(self.x,self.y))
+        pygame.display.update()
+    def animate(self, offx, offy, bob=False):
+        if bob == False:
+            self.x += offx
+            self.y += offy
+        else:
+            self.x += offx
+            uplist = [0,1,2,3,4,15,16,17,18,19]
+            downlist = [5,6,7,8,9,10,11,12,13,14,]
+            if self.state in uplist:
+                self.y -= offy
+            elif self.state in downlist:
+                self.y += offy
+            if self.state == 19:
+                self.state = 0
+            else:
+                self.state += 1
+
+
+
 if __name__ == "__main__":
     pygame.init()
     view = background()
     cb = ConvoBubble('convobubble.PNG', -400, -200)
     panama = real_picture('panama.jpg', 300, 100)
+    cat = sprite('cat-grumpy-icon.png',20,200)
 
 
 
     #view.screen.blit(view.background,(0,0))
     running = True
-
-
+    clock = pygame.time.Clock()
+    FPS = 15
 
 
 
@@ -77,9 +110,14 @@ if __name__ == "__main__":
 
 
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        cat.animate(6,3,bob=True)
+        view.screen.fill(pygame.Color(60,0,245))
+        cat.draw()
+        pygame.time.wait(100)
 
 
 

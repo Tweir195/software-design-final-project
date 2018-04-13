59b049721733a6ef9d197c32a0a3acefc86ff137
Emily Nasiff <emily.nasiff@students.olin.edu>
2018-04-13 14:55:38 -0400

    System Architecture file

--- main.py
+++ main.py
@@ -56,20 +56,82 @@ class real_picture:
         view.screen.blit(self.image,(self.rightl,self.leftl))
         pygame.display.update()
 
+
+class sprite:
+
+
+    def __init__(self,image, x, y):
+        self.image = pygame.image.load(image)
+        self.x = x
+        self.y = y
+        self.state = 0
+    def draw(self):
+        """ intserts real image onto screen"""
+        view.screen.blit(self.image,(self.x,self.y))
+        pygame.display.update()
+    def animate(self, offx, offy, bob=False):
+        if bob == False:
+            self.x += offx
+            self.y += offy
+        else:
+            self.x += offx
+            uplist = [0,1,2,3,4,15,16,17,18,19]
+            downlist = [5,6,7,8,9,10,11,12,13,14,]
+            if self.state in uplist:
+                self.y -= offy
+            elif self.state in downlist:
+                self.y += offy
+            if self.state == 19:
+                self.state = 0
+            else:
+                self.state += 1
+
+class GoToButton(object):
+    """ Places image at the specified location to represent a click zone
+    that takes you to new location"
+
+    Location: string with name of Location it takes you to REMOVED
+    left: leftmost coordinate
+    top: topmost coordinate
+    width: distance from left
+    height: distance from top
+    """
+    def __init__(self, left, top, width = 100, height = 50):
+        # self.location = location
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.surf = pygame.Surface((self.width,self.height))
+        self.color = 255,0,0
+        self.surf.fill(self.color)
+        self.rect = pygame.Rect(self.left,self.top,self.width,self.height)
+        self.rect.width = 0
+
+    def draw(self):
+        """ puts 'button' on screen"""
+        button = pygame.draw.rect(self.surf,self.color,self.rect)
+        view.screen.blit(self.surf,(self.left,self.top))
+        pygame.display.update()
+
+    def check_mouse(self, mouse, color):
+        """checks to see if the cursor is within the button"""
+        if self.left < mouse < self.left+self.width and self.top < mouse < self.top+self.height:
+            self.color = color
+
+
 if __name__ == "__main__":
     pygame.init()
     view = background()
     cb = ConvoBubble('convobubble.PNG', -400, -200)
     panama = real_picture('panama.jpg', 300, 100)
-
-
+    cat = sprite('cat-grumpy-icon.png',20,200)
+    button = GoToButton(1100,780)
 
     #view.screen.blit(view.background,(0,0))
     running = True
-
-
-
-
+    clock = pygame.time.Clock()
+    FPS = 15
 
     view.draw("Attachment-1.png")
     cb.draw()
@@ -77,10 +139,17 @@ if __name__ == "__main__":
 
 
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        cat.animate(6,3,bob=True)
+        view.screen.fill(pygame.Color(60,0,245))
+        cat.draw()
+        button.draw()
+        pygame.time.wait(100)
 
 
 
         #view.clear_screen()"""
+    pygame.quit()

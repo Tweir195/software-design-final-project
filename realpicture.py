532ae4877eacd033dcefd2cc9ee40653e7fd4237
Tweir195 <tommy.weir@students.olin.edu>
2018-04-17 21:16:56 -0400

    Added a non-functional scaling to realpicture.py

--- realpicture.py
+++ realpicture.py
@@ -1,16 +1,21 @@
+
+import pygame
 from background import *
 view = background()
 
-import pygame
 
 class real_picture:
     """Adds a real image on the screen with the initial location
     """
-    def __init__(self,image, right_l, left_l):
-        self.image = pygame.image.load(image)
-        self.rightl = right_l
-        self.leftl = left_l
+    def __init__(self, image, left, top, width=None, height=None, resize=False):
+        image = pygame.image.load(image)
+        if resize is True:
+            self.image = pygame.transform.scale(image, (width, height))
+        else:
+            self.image = image
+        self.left = left
+        self.top = top
     def draw(self):
         """ intserts real image onto screen"""
-        view.screen.blit(self.image,(self.rightl,self.leftl))
+        view.screen.blit(self.image, (self.left, self.top))
         pygame.display.update()

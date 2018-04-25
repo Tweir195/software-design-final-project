582ba9e011c1bc98cee465492c64997597bb3ac2
Tweir195 <tommy.weir@students.olin.edu>
2018-04-25 10:30:43 -0400

    Working background for sprite

--- sprite.py
+++ sprite.py
@@ -7,7 +7,8 @@ class sprite:
     """Creates an image that can be moved across the screen
     """
     def __init__(self,image, x, y, swidth, sheight, backing, width=None, height=None, resize=False):
-        back = pygame.image.load(backing)
+        backing = pygame.image.load(backing)
+        back = pygame.transform.scale(backing, (1280,960))
         image = pygame.image.load(image)
         if resize is True:
             self.image = pygame.transform.scale(image, (width, height))
@@ -37,27 +38,28 @@ class sprite:
         # self.surf.blit(self.image,(-120,-100))
         # view.screen.blit(self.surf,(self.x,self.y))
         # pygame.display.update()
-    def animate(self, offx, offy, moveflag, bob=False):
-        """Moves image across the screen, with offx and offy as the distance
-        in each flip"""
-        if moveflag == False:
-            self.x = self.x
-        else:
-            self.x += offx
-        if bob == False:
-            self.y += offy
-        else:
-            uplist = [0,1,2,3,4,15,16,17,18,19]
-            downlist = [5,6,7,8,9,10,11,12,13,14]
-            if self.state in uplist:
-                self.y -= offy
-            elif self.state in downlist:
-                self.y += offy
-            if self.state == 19:
-                self.state = 0
-            else:
-                self.state += 1
-        return True
+    # def animate(self, offx, offy, moveflag, bob=False):
+    #     """Moves image across the screen, with offx and offy as the distance
+    #     in each flip"""
+    #     if moveflag == False:
+    #         self.x = self.x
+    #     else:
+    #         self.x += offx
+    #     if bob == False:
+    #         self.y += offy
+    #     else:
+    #         uplist = [0,1,2,3,4,15,16,17,18,19]
+    #         downlist = [5,6,7,8,9,10,11,12,13,14]
+    #         if self.state in uplist:
+    #             self.y -= offy
+    #         elif self.state in downlist:
+    #             self.y += offy
+    #         if self.state == 19:
+    #             self.state = 0
+    #         else:
+    #             self.state += 1
+    #     print(self.state)
+    #     return True
 
     def animate_surf(self, offx, offy, moveflag, bob=False):
         """Moves image across the screen, with offx and offy as the distance
@@ -79,4 +81,5 @@ class sprite:
                 self.state = 0
             else:
                 self.state += 1
+        print(self.state)
         return True
